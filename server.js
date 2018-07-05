1aba83a604b389550ae5c6b021070197cb73eabb
dima47 <dimathe47@gmail.com>
2018-07-05 03:06:31 +0300

    index2 2

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', function (req, res) {
       if (err) {
         console.log('Error running count. Message:\n'+err);
       }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.render('index2.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
     res.render('index2.html', { pageCountMessage : null});
